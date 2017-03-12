fb292c1fc2c7cddd889d3f29f1a0969e2edbbe88
KaVaBUnGO <kavabugo1@gmail.com>
2017-03-12 12:50:42 +0500

    added sticker command

--- src/main/java/com/kavabungo/engcoachbot/TestBot.java
+++ src/main/java/com/kavabungo/engcoachbot/TestBot.java
@@ -1,6 +1,8 @@
 package com.kavabungo.engcoachbot;
 
+import org.apache.commons.lang3.StringUtils;
 import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.methods.send.SendSticker;
 import org.telegram.telegrambots.api.objects.Message;
 import org.telegram.telegrambots.api.objects.Update;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
@@ -15,27 +17,49 @@ public class TestBot extends TelegramLongPollingBot {
     public void onUpdateReceived(Update update) {
         // We check if the update has a message and the message has text
         if (update.hasMessage() && update.getMessage().hasText()) {
-            SendMessage message = new SendMessage() // Create a SendMessage object with mandatory fields
-                    .setChatId(update.getMessage().getChatId())
-                    .setText(update.getMessage().getText());
-            try {
-                sendMessage(message); // Call method to send the message
-            } catch (TelegramApiException e) {
-                e.printStackTrace();
+            Message msg = update.getMessage();
+            String answer = checkGreeting(msg);
+            if (StringUtils.isNotBlank(answer)) {
+                sendAnswer(answer, msg.getChatId());
+                return;
             }
+            checkTime(msg);
         }
-        if (update.hasChannelPost()) {
-            Message channelPost = update.getChannelPost();
-            SendMessage message = new SendMessage() // Create a SendMessage object with mandatory fields
-                    .setChatId(channelPost.getChatId())
-                    .setText("[EngCoachBot]: " + channelPost.getText());
+    }
+
+    private void checkTime(Message msg) {
+        String msgText = msg.getText().toLowerCase();
+        if (msgText.toLowerCase().contains("time")) {
+            SendSticker sticker = new SendSticker().setSticker("BQADAgADeAcAAlOx9wOjY2jpAAHq9DUC").setChatId(msg.getChatId());
             try {
-                sendMessage(message); // Call method to send the message
+                sendSticker(sticker);
             } catch (TelegramApiException e) {
                 e.printStackTrace();
             }
         }
     }
+
+    private String checkGreeting(Message msg) {
+        String msgText = msg.getText().toLowerCase();
+        String msgUserName = msg.getFrom().getUserName() != null ? msg.getFrom().getUserName() : msg.getFrom().getFirstName();
+        String answer = StringUtils.EMPTY;
+        if (msgText.equals("hi") || msgText.equals("прив") || msgText.equals("привет")) {
+            answer = msg.getText() + ", " + msgUserName + "! :3";
+        }
+        return answer;
+    }
+
+    private void sendAnswer(String answer, Long chatId) {
+        SendMessage message = new SendMessage() // Create a SendMessage object with mandatory fields
+                .setChatId(chatId)
+                .setText(answer);
+        try {
+            sendMessage(message); // Call method to send the message
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
 
     @Override
     public String getBotUsername() {
